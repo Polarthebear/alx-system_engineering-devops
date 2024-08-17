93297a8c6060206ab7895f103933aba632b74352
Polarthebear <thatotae325@gmail.com>
2024-08-17 12:54:31 -0700

    Task 0.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,11 +3,16 @@
 
 import requests
 
+#!/usr/bin/python3
+"""Function that queries Reddit API & returns the number of subscribers."""
+
+import requests
+
 
 def number_of_subscribers(subreddit):
     """Return total subreddit subscribers."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "Polarbear325"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
